b46e58824fd62d0e5ace9fcc61123288d7831aa5
Hopac1 <chris1101@hotmail.co.uk>
2022-01-05 23:44:10 +0000

    give classes to elements

--- contact.js
+++ contact.js
@@ -2,10 +2,12 @@ function createContactPage(divContent) {
     let contactDiv = document.createElement("div");
 
     let email = document.createElement("p");
-    email.textContent = "email: commandershepard@codex.com";
+    email.textContent = "email: mustnotbenamed@email.com";
+    email.className = "email";
 
     let address = document.createElement("p");
-    address.textContent = "address: 123 Example Road, London, England";
+    address.textContent = "93 Diagon Alley, London, England, Great Britain";
+    address.className = "address";
 
     contactDiv.appendChild(email);
     contactDiv.appendChild(address);
