f3ff8f41fe2c748c16ea305766c9ac7c77e112c6
Hopac1 <chris1101@hotmail.co.uk>
2022-01-05 11:39:59 +0000

    add factory function for creating menu items

--- dist/menu.js
+++ dist/menu.js
@@ -1,5 +1,44 @@
 function createMenuPage(divContent) {
-    console.log("menu page")
+    const menuContainer = document.createElement("div");
+    menuContainer.className = "menu-container";
+
+    let menuItem = createMenuItem("pumpkin-pasties", ".jpg");
+    menuContainer.appendChild(menuItem);
+
+    menuItem = createMenuItem("hogwarts-crest-pie", ".jpg");
+    menuContainer.appendChild(menuItem);
+
+    menuItem = createMenuItem("chocolate-frog", ".png");
+    menuContainer.appendChild(menuItem);
+
+
+    menuItem = createMenuItem("bertie-botts-beans", ".png");
+    menuContainer.appendChild(menuItem);
+
+    menuItem = createMenuItem("butterbeer", ".png");
+    menuContainer.appendChild(menuItem);
+
+    menuItem = createMenuItem("exploding-bon-bons", ".png");
+    menuContainer.appendChild(menuItem);
+
+    menuItem = createMenuItem("fizzing-whizzbees", ".png");
+    menuContainer.appendChild(menuItem);
+
+
+    divContent.appendChild(menuContainer);
 };
 
-export {createMenuPage};
+function createMenuItem(urlName, format) {
+    const image = document.createElement("img");
+    image.className = "menu-item";
+    image.src = "/dist/images/" + urlName + format;
+    image.alt = urlName + "-image";
+    return image;
+};
+
+function createItemContainer() {
+    const container = document.createElement("div");
+    
+}
+
+export {createMenuPage, createMenuItem};
