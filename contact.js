ccff2b8018cce1d32320f06d40a30cfeef6cc1b5
Hopac1 <chris1101@hotmail.co.uk>
2022-01-06 21:28:28 +0000

    add in map of shop location

--- contact.js
+++ contact.js
@@ -1,16 +1,33 @@
 function createContactPage(divContent) {
     let contactDiv = document.createElement("div");
+    contactDiv.className = "contact-container";
+
+    let contactTitle = document.createElement("p");
+    contactTitle.textContent = "Contact Us";
+    contactTitle.className = "title";
 
     let email = document.createElement("p");
-    email.textContent = "email: mustnotbenamed@email.com";
-    email.className = "email";
+    email.textContent = "mustnotbenamed@email.com";
+    email.className = "contact";
+
+    let addressTitle = document.createElement("p");
+    addressTitle.textContent = "Where We Are";
+    addressTitle.className = "title";
 
     let address = document.createElement("p");
     address.textContent = "93 Diagon Alley, London, England, Great Britain";
-    address.className = "address";
+    address.className = "contact";
 
+    const image = document.createElement("img");
+    image.className = "map";
+    image.src = "images/map.png";
+    image.alt = "map-of-shop";
+
+    contactDiv.appendChild(contactTitle);
     contactDiv.appendChild(email);
+    contactDiv.appendChild(addressTitle);
     contactDiv.appendChild(address);
+    contactDiv.appendChild(image);
 
     divContent.appendChild(contactDiv);
 }
